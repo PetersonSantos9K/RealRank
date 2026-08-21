3c2d860703fc6a8e7f52498afcd6492e70f67ec6
PetersonSantos9K <petersonsantos9k@gmail.com>
2026-08-21 18:21:14 -0300

    refactor: migração da estrutura do projeto para Spring Boot

---
+++ real-rank-api/src/main/java/com/peterson/realrankapi/RealRankApiApplication.java
@@ -0,0 +1,13 @@
+package com.peterson.realrankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealRankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealRankApiApplication.class, args);
+	}
+
+}
---
+++ real-rank-api/src/test/java/com/peterson/realrankapi/RealRankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterson.realrankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealRankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
